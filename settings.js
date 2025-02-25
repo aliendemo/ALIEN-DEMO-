ee56a0414f06fe28f83e44ae02883365f5d78192
aliendemo <bfoday023@gmail.com>
2025-02-25 13:44:53 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Knight,
+  author: 'Alien',
+  botName: "Alien Bot",
+  botOwner: 'Alien', // Your name
+  ownerNumber: '+23273314623', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
